66c38f3cfedc3ab760c9441f47825560eae9e351
Bálint Bujtor <balint.bujtor@gmail.com>
2023-05-18 18:21:13 +0200

    added generative models

---
+++ generativeModels/generativeModels.py
@@ -0,0 +1,159 @@
+import numpy as np
+import scipy
+from helpers import helpers
+
+
+def logMVG(traindata: np.ndarray, trainlabel: np.ndarray, testdata: np.ndarray, nclasses: int, prior : np.ndarray) -> tuple[np.ndarray, np.ndarray]:   
+    """
+    Multivariate Gaussian Classifier that computes the posterior probability
+    for a given dataset and prior probability.
+
+    This implementation is a more robust one of MVG that uses logs
+    to avoid numeric issues.
+
+    Args:
+        traindata (np.ndarray): data on which to train the model
+        trainlabel (np.ndarray): labels for the training set
+        testdata (np.ndarray): data on which to evaluate the model
+        nclasses (int): number of classes in the dataset
+        prior (np.ndarray): prior probabilities in the dataset
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: returns the Joint probability and the posterior probability
+    """
+    logscore = []
+    
+    for i in range(0, nclasses):
+        trainClassSample = traindata[:, trainlabel==i]
+
+        mu, cov = helpers.ML_estimates(trainClassSample) 
+        logpdf = helpers.logpdf_GAU_ND(testdata, mu, cov)
+        
+        logscore.append(helpers.vrow(logpdf))
+        
+    logscore = np.vstack(logscore)
+    
+    logSJoint = logscore + np.log(prior)
+    logSMarginal = helpers.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    
+    
+    return np.exp(logSJoint), np.exp(logSPost)    
+
+
+def logNaiveBayes(traindata: np.ndarray, trainlabel: np.ndarray, testdata: np.ndarray, nclasses: int, prior : np.ndarray) -> tuple[np.ndarray, np.ndarray]:   
+    """
+    Naive Bayes Gaussian Classifier
+
+    Args:
+        traindata (np.ndarray): data on which to train the model
+        trainlabel (np.ndarray): labels for the training set
+        testdata (np.ndarray): data on which to evaluate the model
+        nclasses (int): number of classes in the dataset
+        prior (np.ndarray): prior probabilities in the dataset
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: returns the Joint probability and the posterior probability
+    """
+    
+    
+    logscore = []
+    for i in range(0, nclasses):
+        trainClassSample = traindata[:, trainlabel==i]
+
+        mu, cov = helpers.ML_estimates(trainClassSample)
+        id = np.identity(cov.shape[1])
+        naivecov = cov * id
+        logpdf = helpers.logpdf_GAU_ND(testdata, mu, naivecov)
+        logscore.append(helpers.vrow(logpdf))
+        
+    logscore = np.vstack(logscore)
+    
+    logSJoint = logscore + np.log(prior)
+    logSMarginal = helpers.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    
+    return np.exp(logSJoint), np.exp(logSPost)
+
+
+def logTiedMVG(traindata: np.ndarray, trainlabel: np.ndarray, testdata: np.ndarray, nclasses: int, prior : np.ndarray) -> tuple[np.ndarray, np.ndarray]:   
+    """
+    Computes the the tied Multivariate Gaussian Density for a given dataset
+    Tied: only one global covariance matrix that is summed and averaged of the class covs
+    Log: more robust approach
+    Args:
+        traindata (np.ndarray): data on which to train the model
+        trainlabel (np.ndarray): labels for the training set
+        testdata (np.ndarray): data on which to evaluate the model
+        nclasses (int): number of classes in the dataset
+        prior (np.ndarray): prior probabilities in the dataset
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: returns the Joint probability and the posterior probability
+    """
+    logscore = []    
+    mus = []
+    tied_cov= np.zeros((traindata.shape[0], traindata.shape[0]), dtype = float)
+
+    for i in range(0, nclasses):
+        trainClassSample = traindata[:, trainlabel==i]
+
+        mu, cov = helpers.ML_estimates(trainClassSample)
+        mus.append(mu)
+        tied_cov += cov*trainClassSample.shape[1]
+
+    tied_cov /= traindata.shape[1]
+
+    for i in range(0, nclasses):
+        logpdf = helpers.logpdf_GAU_ND(testdata, mus[i], tied_cov)
+
+        logscore.append(helpers.vrow(logpdf))
+
+    logscore = np.vstack(logscore)
+    logSJoint = logscore + np.log(prior)
+    logSMarginal = helpers.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    
+    return np.exp(logSJoint), np.exp(logSPost)
+
+
+def logTiedNaiveBayes(traindata: np.ndarray, trainlabel: np.ndarray, testdata: np.ndarray, nclasses: int, prior : np.ndarray) -> tuple[np.ndarray, np.ndarray]:  
+    """
+    Tied Naive Bayes Gaussian Classifier
+
+    Args:
+        traindata (np.ndarray): data on which to train the model
+        trainlabel (np.ndarray): labels for the training set
+        testdata (np.ndarray): data on which to evaluate the model
+        nclasses (int): number of classes in the dataset
+        prior (np.ndarray): prior probabilities in the dataset
+
+    Returns:
+        tuple[np.ndarray, np.ndarray]: returns the Joint probability and the posterior probability
+    """
+
+    logscore = []    
+    mus = []
+    tied_cov= np.zeros((traindata.shape[0], traindata.shape[0]), dtype = float)
+
+    for i in range(0, nclasses):
+        trainClassSample = traindata[:, trainlabel==i]
+        
+        mu, cov = helpers.ML_estimates(trainClassSample)
+        id = np.identity(cov.shape[1])
+        naivecov = cov * id
+        mus.append(mu)
+        tied_cov += naivecov*trainClassSample.shape[1]
+    
+    tied_cov /= traindata.shape[1]
+    
+    for i in range(0, nclasses):        
+        logpdf = helpers.logpdf_GAU_ND(testdata, mus[i], tied_cov)
+        logscore.append(helpers.vrow(logpdf))
+        
+    logscore = np.vstack(logscore)
+    logSJoint = logscore + np.log(prior)
+    logSMarginal = helpers.vrow(scipy.special.logsumexp(logSJoint, axis=0))
+    logSPost = logSJoint - logSMarginal
+    
+    return np.exp(logSJoint), np.exp(logSPost)
--- helpers/helpers.py
+++ helpers/helpers.py
@@ -9,6 +9,31 @@ def vrow(array):
 
     return array.reshape((1, array.shape[0]))
 
+def logpdf_GAU_ND(x : np.ndarray, mu :np.ndarray, C :np.ndarray) -> np.ndarray:
+    """computes the log density of the dataset x given mu and C
+    
+    M is the size of the feature vector
+
+    Args:
+        x (np.ndarray): dataset
+        mu (np.ndarray): mean that is of shape (M, 1)
+        C (np.ndarray): covariance matrix that is of shape (M, M)
+
+    Returns:
+        np.ndarray: log density
+    """
+
+    constant = 0.5*x.shape[0]*np.log(2*np.pi)
+    logdetc = 0.5 * np.linalg.slogdet(C)[1]
+    
+    xc = x - vcol(mu)
+    first_minor = np.dot(np.linalg.inv(C), xc) # cov*centered data, in case of iris 4x150
+    v = xc*first_minor # element-wise multiplication (as in vector by vector), in case of iris yields 4x150
+    final = 0.5 * v.sum(0) # sum over columns --> in case of iris yields 1x150
+
+    return np.asarray(- constant - logdetc - final, dtype=float)
+
+
 def ML_estimates(XND : np.ndarray) :
     """returns the estimated mu and cov
 
@@ -19,3 +44,71 @@ def ML_estimates(XND : np.ndarray) :
     mu_ML = vcol(XND.sum(1) / XND.shape[1])
     cov_ML = np.dot((XND - mu_ML), (XND - mu_ML).T)/XND.shape[1]
     return mu_ML, cov_ML
+
+
+def splitToKFold(nsamples : int, nfolds=10, LOO=False) -> tuple[int, np.ndarray]:
+    """
+    Splits the dataset based on the number of samples into nfolds (almost) equal parts
+    
+    If LOO is true, then K-Folds becomes Leave-One-Out
+
+    Args:
+        nsamples (int): number of samples in the dataset
+        nfolds (int, optional): number of folds, the function split the data into nfolds parts. Defaults to 10.
+        LOO (bool, optional): if True, nfolds = nsamples --> LOO method. Defaults to False.
+
+    Returns:
+        tuple[int, np.ndarray]: 
+            the number of splits in the dataset (iterations to perform)
+            the bounds of the indeces for the split
+    """
+    if(LOO == True):
+        nFolds = nsamples
+    elif(nfolds > nsamples):
+        nFolds = nsamples
+    else:
+        nFolds = nfolds
+    
+    foldBounds = np.full(nFolds, nsamples // nFolds, dtype=int)
+    foldBounds[: nsamples % nFolds] += 1
+        
+    for i in range(1, nFolds):
+        foldBounds[i] += foldBounds[i - 1]
+    
+    nsplits = foldBounds.size
+    return  nsplits, foldBounds
+
+
+def getCurrentKFoldSplit(data : np.ndarray, labels : np.ndarray, iter : np.ndarray, foldBounds : np.ndarray):
+    """
+    Splits the dataset based on the iteration and the foldbounds into traint and test
+
+    Args:
+        data (np.ndarray): dataset
+        labels (np.ndarray): labels of the dataset
+        iter (np.ndarray): number of K-fold iteration
+        foldBounds (np.ndarray): bounds of indeces for the split
+
+    Returns:
+        the split up train and test set
+    """
+    if(iter == 0):
+        lowerbound = 0
+        upperbound = foldBounds[iter]
+
+        testdata = data[:, lowerbound : upperbound]
+        testlabels = labels[ lowerbound : upperbound]
+        
+        traindata =data[:, upperbound :]
+        trainlabels =labels[upperbound :]
+    else:
+        lowerbound = foldBounds[iter - 1]
+        upperbound = foldBounds[iter]
+
+        testdata = data[:, lowerbound : upperbound]
+        testlabels = labels[ lowerbound : upperbound]
+        
+        traindata = np.concatenate((data[:, : lowerbound], data[:, upperbound :]), axis=1)
+        trainlabels =np.concatenate((labels[ : lowerbound], labels[upperbound :]))
+    
+    return (traindata, trainlabels), (testdata, testlabels) 
